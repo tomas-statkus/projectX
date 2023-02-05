57e540868eade5c434e21af4e6dab73cba78500d
unknown <tomasstatkus@hotmail.com>
2023-02-05 22:06:15 +0000

    Revert "Revert "initial commit""
    This reverts commit f0180e2c873e8973cf9190234a393a090b4fe1bc.

---
+++ src/main/kotlin/scraperOne.kt
@@ -0,0 +1,51 @@
+import org.jsoup.Jsoup
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Path
+
+fun main(){
+    ScraperOne().runScraper()
+    //scraperOne().runScraper().forEach { println(it) }
+}
+
+
+class ScraperOne {
+    fun runScraper() {
+        // this object connects to the website
+
+
+        val url = "https://www.baltic-legal.com/banking-in-lithuania-list-of-banks-eng.htm"
+        val jsoupConnection = Jsoup.connect(url)
+
+        //this catches all the h2 tags in the body of the loaded page in a list "getElementsByTag"
+        val h4Tags = jsoupConnection.get().body().getElementsByClass("content")[0].getElementsByTag("li")
+
+        //println(h4Tags)
+
+        // the main array list of hashmaps
+        val arrayOfMap = ArrayList<HashMap<String, String>>()
+
+        // loop through the list of h1 tags
+        h4Tags.forEach {
+            //map to collect information
+            val map = HashMap<String, String>()
+            //insert information to the hashmaps
+
+            val href = it.getElementsByTag("a").attr("href")
+            val title = it.getElementsByTag("a").attr("title")
+
+            println("$title")
+
+            map["hello"] = title
+            //insert the map into a list of hashmaps
+            arrayOfMap.add(map)
+        }
+
+
+        File("somefile.txt").printWriter().use { out ->
+            arrayOfMap.forEach {
+                out.println(it["hello"])
+            }
+        }
+
+    }}
